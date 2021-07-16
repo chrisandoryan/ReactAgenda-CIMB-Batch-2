fba7742b7f673ebabd41dba2723ec1e44d8a5fc2
Chrisando Ryan <chrisando.siahaan@gmail.com>
2021-07-16 14:40:56 +0700

    finish implementing all required unit tests

--- src/pages/__tests__/agendaClass.test.js
+++ src/pages/__tests__/agendaClass.test.js
@@ -3,11 +3,21 @@ import AgendaClass from "../AgendaClass";
 
 
 describe("Agenda Page (Class)", () => {
-    it("should have 2 agendas in its state", () => {
-        let component = shallow(<AgendaClass />);
+    var component = shallow(<AgendaClass />);
+    it("should have 2 agendas in its initial state", () => {
         expect(component.state().agendas.length).toBe(2);
     });
-    it("should delete agenda with id 1 when deleteAgenda is called", () => {
-        
-    })
+    it("should delete agenda with id 0 when deleteAgenda is called", () => {
+        component.instance().deleteAgenda(0);
+        expect(component.state().agendas.length).toBe(1);
+    });
+    it("should add new agenda when addAgenda is called", () => {
+        component.instance().addAgenda({
+            agendaTitle: "Testing Add Agenda",
+            agendaDate: "16 Juli 2021",
+            agendaTime: "12.00",
+            agendaDesc: "Unit Test Agenda Page (Class)"
+        });
+        expect(component.state().agendas.length).toBe(2);
+    });
 })
